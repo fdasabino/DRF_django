1e3faeb560449d3c773b60d0e83d5b02f068cc25
Francisco Sabino <fdasabino@gmail.com>
2021-12-25 00:11:07 +0100

    commit 3

--- core/settings.py
+++ core/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     "blog",
     "blog_api",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -130,3 +132,5 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.AllowAny",
     ]
 }
+
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000", "http://localhost:3000"]
