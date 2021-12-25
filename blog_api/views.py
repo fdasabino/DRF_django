aebb8dd2df0c0ee6e9281e03b8582a8f5bf81944
Francisco Sabino <fdasabino@gmail.com>
2021-12-25 10:52:03 +0100

    commit 4

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,3 +1,30 @@
+from rest_framework import generics
+from blog.models import Post
+from .serializers import PostSerializer
+from rest_framework.permissions import *
+
+
+class PostUserWritePermission(BasePermission):
+    message = "Editing posts is restricted to the author only."
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
+
+class PostList(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView, PostUserWritePermission):
+    permission_classes = [PostUserWritePermission]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
 """ Concrete View Classes
 #CreateAPIView
 Used for create-only endpoints.
@@ -18,17 +45,3 @@ Used for read or delete endpoints to represent a single model instance.
 #RetrieveUpdateDestroyAPIView
 Used for read-write-delete endpoints to represent a single model instance.
 """
-
-from rest_framework import generics
-from blog.models import Post
-from .serializers import PostSerializer
-
-
-class PostList(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-class PostDetail(generics.RetrieveDestroyAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
--- core/settings.py
+++ core/settings.py
@@ -127,9 +127,16 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+# Rest Framework Permissions:
+
+# AllowAny
+# IsAuthenticated
+# IsAdminUser
+# IsAuthenticatedOrReadOnly
+
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.AllowAny",
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ]
 }
 
