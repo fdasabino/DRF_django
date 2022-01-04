9762771b8ada0c7c9df16cc1f04ce2a979760a2f
Francisco Sabino <fdasabino@gmail.com>
2022-01-04 18:19:07 +0100

    commit 10

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,11 +1,16 @@
 from django.db import router
-from blog.urls import urlpatterns
-from .views import PostList
+from django.urls import path
+from .views import *
 from rest_framework.routers import DefaultRouter
 
 app_name = "blog_api"
 
-router = DefaultRouter()
-router.register("", PostList, basename="post")
+# router = DefaultRouter()
+# router.register("", PostList, basename="post")
+# urlpatterns = router.urls
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("posts/", PostDetail.as_view(), name="detailcreate"),
+    path("search/", PostSearch.as_view(), name="postsearch"),
+    path("", PostList.as_view(), name="listcreate"),
+]
